88169a061171b68356b421cff997a2fea606c777
DANILO VIACAVA <danilo.viacava@inkafarmadigital.pe>
2019-11-18 23:46:32 -0500

    add banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './core/home/components/generics/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
